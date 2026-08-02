63e6977a2a77d94306470fe768b6731e2cb93aa7
dhdmanikanta <231fa16016@vignan.ac.in>
2026-08-02 13:30:35 +0530

    Add aggregate function examples

---
+++ aggregate_functions.sql
@@ -0,0 +1,11 @@
+SELECT COUNT(*) AS TotalPatients
+FROM Patients;
+
+SELECT AVG(Age) AS AverageAge
+FROM Patients;
+
+SELECT MAX(Age) AS OldestPatient
+FROM Patients;
+
+SELECT MIN(Age) AS YoungestPatient
+FROM Patients;
